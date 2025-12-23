c86d3e82c42db0939105bac338d3af0721110d7d
Hammad2346 <HammadArif14244@gmail.com>
2025-12-23 16:07:36 +0500

    auth page login

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,59 @@
+import { Input } from "@/components/ui/input"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import LoginForm from "@/components/loginForm"
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex justify-center items-center ">
+      <Tabs defaultValue="login" className="w-[420px] space-y-8 ">
+        
+        {/* Header */}
+        <TabsContent value="login" className="gap-2 flex flex-col justify-center items-center">
+          <h1 className="font-bold text-5xl text-primary">Login</h1>
+          <p className="text-sm text-muted-foreground">
+            Welcome back! Please log in to access your account.
+          </p>
+        </TabsContent>
+
+        <TabsContent value="signup" className="gap-2 flex flex-col justify-center items-center">
+          <h1 className="font-bold text-5xl text-primary">Sign in</h1>
+          <p className="text-sm text-muted-foreground text-center">
+            Join our community today! Create an account to unlock exclusive features and personalized experiences.
+          </p>
+        </TabsContent>
+
+        <div className="w-full flex justify-center">
+        <TabsList className="border border-[#cbcbcb] bg-[#282828] w-50 py-2">
+          <TabsTrigger value="login" className="flex-1">
+            Log in
+          </TabsTrigger>
+          <TabsTrigger value="signup" className="flex-1">
+            Sign up
+          </TabsTrigger>
+        </TabsList>
+        </div>
 
 
-export default function Page(){
+       <TabsContent value="login">
+            <LoginForm/>
+        </TabsContent>
 
-    return (
-        <>
-        </>
-    )
+  
+        <TabsContent value="signup">
+          <div className="space-y-6">
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <Input type="password" placeholder="Confirm Password" />
+
+            <Button className="w-full bg-primary text-secondary">
+              Sign up
+            </Button>
+          </div>
+        </TabsContent>
+
+      </Tabs>
+    </div>
+  )
 }
