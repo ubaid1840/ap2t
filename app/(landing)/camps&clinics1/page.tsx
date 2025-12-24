dd54bb1b8c6282f25b1a7410337d6d5c69e20589
Hammad2346 <HammadArif14244@gmail.com>
2025-12-24 16:01:00 +0500

    backup pages for inhouse and camps added

---
+++ app/(landing)/camps&clinics1/page.tsx
@@ -0,0 +1,175 @@
+import { Card, CardContent } from "@/components/ui/card"
+import { Calendar, Clock, Users } from "lucide-react"
+
+export default function Page(){
+    return(
+        <div className="flex flex-col gap-30">
+        <section className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 sm:py-2 ">
+        <div className="relative mt-40 flex flex-col items-center bg-[#090909]">
+
+            {/* blurry blob */}
+          <div className="absolute top-0 left-30% bg-primary h-60 w-50 rounded-full blur-[220px] z-0"></div>
+
+
+
+          <div className="z-10 gap-8 px-10 w-full flex flex-col items-center">
+            <div className="flex flex-col w-full items-center gap-4 Web/letter-spacing/0">
+              <h1 className="text-5xl ">Camps & Clinics</h1>
+              <p className="text-sm text-muted">
+                Enhance your reaction time, coordination, and move
+              </p>
+            </div>
+
+            <img
+              src="./contact.jpg"
+              alt=""
+              className="w-full h-80 object-fit rounded"
+            />
+          </div>
+        </div>
+      </section>
+    <section className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 sm:py-2 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full">
+        {campsData.map((camp,i)=>{return(
+            <Card key={i} className="bg-[#131313] rounded-[6px]">
+                <CardContent className="space-y-3">
+                    <h1 className="text-xl font-semibold">{camp.title}</h1>
+                    <div className="flex items-center gap-2"><span className="text-primary">{camp.date.icon}</span> <span className="text-sm text-muted">{camp.date.text}</span></div>
+                    <div className="flex items-center gap-2"><span className="text-primary">{camp.time.icon}</span>  <span className="text-sm text-muted">{camp.time.text}</span></div>
+                    <div className="flex items-center gap-2"><span className="text-primary">{camp.ages.icon}</span>  <span className="text-sm text-muted">{camp.ages.text}</span></div>
+                    <div className="flex items-center gap-2"><span className="text-primary">{camp.ages.icon}</span>  <span className="text-sm text-muted">{camp.ages.text}</span></div>
+                    <h1 className="text-primary text-xl font-semibold">{camp.priceDuration}</h1>
+                    <p className="text-sm text-muted">{camp.description}</p>
+                    <h1>What's included:</h1>
+                    <ul className="list-disc space-y-3 pl-5 marker:text-primary text-sm text-muted ">
+                        
+                        {camp.WhatsIncluded.map((offer,i)=>{
+                        return(
+                            
+                            <li key={i}>{offer}</li>
+
+                        
+                        )
+                    })}</ul>
+
+
+
+
+                </CardContent>
+            </Card>
+        )})}
+      </div>
+        </section>
+        </div>
+        
+    )
+}
+
+const campsData=[{
+    title:"Winter Break Camp",
+    date:{
+        icon:<Calendar/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    time:{
+        icon:<Clock/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    ages:{
+        icon:<Users/>,
+        text:"Ages 7-16"
+    },
+    priceDuration:"$160 (4 days)",
+    description:"Full day intensive training camp during winter break, Focus on technical skills, small-side games,and fitness",
+    WhatsIncluded:["4 full days of training","Technical skill development","Imall-sided games",
+        "lunch break included (bring lunch)","Indoor facility"]
+
+
+},
+
+
+{
+    title:"Winter Break Camp",
+    date:{
+        icon:<Calendar/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    time:{
+        icon:<Clock/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    ages:{
+        icon:<Users/>,
+        text:"Ages 7-16"
+    },
+    priceDuration:"$160 (4 days)",
+    description:"Full day intensive training camp during winter break, Focus on technical skills, small-side games,and fitness",
+    WhatsIncluded:["4 full days of training","Technical skill development","Imall-sided games",
+        "lunch break included (bring lunch)","Indoor facility"]
+
+
+},
+{
+    title:"Winter Break Camp",
+    date:{
+        icon:<Calendar/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    time:{
+        icon:<Clock/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    ages:{
+        icon:<Users/>,
+        text:"Ages 7-16"
+    },
+    priceDuration:"$160 (4 days)",
+    description:"Full day intensive training camp during winter break, Focus on technical skills, small-side games,and fitness",
+    WhatsIncluded:["4 full days of training","Technical skill development","Imall-sided games",
+        "lunch break included (bring lunch)","Indoor facility"]
+
+
+},
+{
+    title:"Winter Break Camp",
+    date:{
+        icon:<Calendar/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    time:{
+        icon:<Clock/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    ages:{
+        icon:<Users/>,
+        text:"Ages 7-16"
+    },
+    priceDuration:"$160 (4 days)",
+    description:"Full day intensive training camp during winter break, Focus on technical skills, small-side games,and fitness",
+    WhatsIncluded:["4 full days of training","Technical skill development","Imall-sided games",
+        "lunch break included (bring lunch)","Indoor facility"]
+
+
+},
+{
+    title:"Winter Break Camp",
+    date:{
+        icon:<Calendar/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    time:{
+        icon:<Clock/>,
+        text:"9:00 AM - 3:00 PM"
+    },
+    ages:{
+        icon:<Users/>,
+        text:"Ages 7-16"
+    },
+    priceDuration:"$160 (4 days)",
+    description:"Full day intensive training camp during winter break, Focus on technical skills, small-side games,and fitness",
+    WhatsIncluded:["4 full days of training","Technical skill development","Imall-sided games",
+        "lunch break included (bring lunch)","Indoor facility"]
+
+
+},
+]
---
+++ app/(landing)/inhouseprograms1/page.tsx
@@ -0,0 +1,183 @@
+import Booking from "@/components/home-page/book.client";
+import { Card, CardContent,CardFooter } from "@/components/ui/card";
+import GradientIcon from "@/components/icon-container";
+import { Trophy, Volleyball } from "lucide-react"
+import { Button } from "@/components/ui/button";
+
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-30">
+      <section className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8  ">
+        <div className="relative mt-40 flex flex-col items-center bg-[#090909]">
+          <div className="absolute top-0 left-30% bg-primary h-60 w-50 rounded-full blur-[220px] z-0"></div>
+
+
+
+          <div className="z-10 space-y-8">
+            <div className="flex flex-col w-full items-center gap-4 Web/letter-spacing/0">
+              <h1 className="text-5xl ">Programs at Our Facility</h1>
+              <p className="text-sm text-muted">
+                Enhance your reaction time, coordination, and movement
+                efficiency.
+              </p>
+            </div>
+
+            <img
+              src="./inhouse1hero.jpg"
+              alt=""
+              className="w-full h-80 object-contain rounded"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="max-w-7xl mx-auto w-full lg:px-30  ">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-3xl">Class Overview</h1>
+          <p className="text-sm text-muted">
+            Our Agility & Quickness Drills class is built to improve your
+            athleticism by improving reaction time, directional changes, and
+            multi-directional speed. Through cutting drills, cone work, and
+            explosive reaction exercises, you'll sharpen your footwork and
+            enhance your ability to change direction efficiently. All exercises
+            are sport-specific and scalable, meaning they can be customized to
+            your skill level and athletic goals. Whether you're an elite athlete
+            or someone just looking to move more confidently, this class will
+            challenge your mind-body connection and your coordination.
+          </p>
+        </div>
+      </section>
+
+      <section className="max-w-7xl w-full  ">
+        <div className="flex items-center bg-[#1E1E1E] gap-16 p-10 lg:px-30">
+          <div className="gap-12 flex flex-col flex-1">
+            <div className="space-y-6">
+              <h1 className="text-2xl font-bold">Key Benefits</h1>
+              <ul className="list-disc space-y-3 pl-5 marker:text-primary text-sm text-muted ">
+                <li>Improve agility and overall speed</li>
+                <li>Enhance coordination and body control</li>
+                <li>Increase coordination and acceleration speed</li>
+                <li>
+                  Move confidently and accelerate faster in game situations
+                </li>
+                <li>Build muscle memory for reactive movements</li>
+              </ul>
+            </div>
+
+            <div className="space-y-6">
+              <h1 className="text-2xl font-bold">
+                Who This Program is Perfect For
+              </h1>
+              <ul
+                className="list-disc space-y-3 pl-5 marker:text-primary text-sm text-muted "
+              >
+                <li>
+                  Competitive athletes in sports like soccer, basketball,
+                  tennis, and football
+                </li>
+                <li>
+                  Youth athletes experiencing foundational athletic skills
+                </li>
+                <li>Adults seeking improved coordination and balance</li>
+                <li>
+                  Anyone seeking to enhance sports-related quickness and agility
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex flex-1">
+            <img className=" object-contain" src="./keyfeature.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+
+        <section className="max-w-7xl w-full lg:px-30 ">
+          <div className="flex flex-col items-center w-full mx-auto">
+            <p className="font-bold text-3xl sm:text-4xl mb-2 text-center">In-House Events</p>
+            <p className="text-[#B3B3B3] mb-10 max-w-2xl text-center text-base sm:text-lg">
+              Comprehensive training programs designed to develop exceptional soccer players
+            </p>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full">
+              {events.map((item, i) => (
+                <Card
+                  className="flex gap-4 bg-[#131313] justify-between rounded-lg p-4"
+                  key={i}
+                >
+                  <CardContent>
+                    <div className="flex flex-col items-center gap-4">
+                      <GradientIcon>{item.icon}</GradientIcon>
+                      <p className="text-center font-semibold mb-2 text-xl sm:text-2xl">{item.title}</p>
+                      <p className="text-center text-[#B3B3B3] text-sm sm:text-base leading-relaxed">
+                        {item.description}
+                      </p>
+                    </div>
+                  </CardContent>
+                  <CardFooter className="h-20 flex justify-between items-center border-t border-[#282828]">
+                    <h1 className="text-primary">{item.price}/hr</h1>
+                    <Button className="bg-primary text-secondary px-10">
+                        Book Now
+                    </Button>
+                  </CardFooter>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </section>
+
+
+      <section className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 ">
+        
+            <Booking />
+        
+      </section>
+    </div>
+  );
+}
+
+const events = [
+  {
+    title: "Speed, Agility, and Quickness",
+    description:
+      "At AP2T, we recognize that today’s athletes are faster, quicker, and more agile than ever before. Since the difference between the best and the rest can often be fractions of seconds, we understand that developing game-changing speed and agility is one of the most important elements of our training regimen. Creating athletes who are strong and skillful is not enough to dominate the competition, which is why AP2T focuses heavily on executing sport-specific movements as explosively as possible. At AP2T, you will become a student of linear, lateral, and change-of-direction techniques. Through detailed instruction that explains the intricacies of elite-level running, AP2T athletes will improve acceleration, top speed, quickness, and enhance their resistance to injuries commonly associated with sport-related running.",
+    icon: <img src="/inhouse/running.png" className="w-8 h-8" />,
+    price:40
+  },
+  {
+    title: "Strength Training",
+    description:
+      "AP2T prides itself on offering a strength and conditioning program that is second to none. We are committed to providing each student-athlete with the most appropriate exercise regimen to prepare them for their specific sport. Through the implementation of properly prescribed strength, power, plyometrics, speed, endurance, and flexibility exercises, we induce positive gains in holistic wellness, overall fitness, and resistance to injury as it relates to sport performance. AP2T creates individualized, sport-specific, multi-planar training regimens designed to produce morphological and neurological adaptations that yield physiological advantages for athletic performance. Our research-based programs form the foundation of AP2T’s Strength & Conditioning approach.",
+    icon: <img src="/inhouse/gym.png" className="w-8 h-8" />,
+    price:40
+  },
+  {
+    title: "Nutrition",
+    description:
+      "You are what you eat! In terms of athletic performance and general fitness, no truer words have been spoken. AP2T offers customized nutritional programs for every age and level of athleticism. Whether your goal is to build lean muscle or burn fat, AP2T’s professional staff will provide you with the nutritional foundation needed to help you achieve your goals.",
+    icon: <img src="/inhouse/diet.png" className="w-8 h-8 text-primary" />,
+    price:40
+  },
+  {
+    title: "Sport-Specific Technical Training",
+    description:
+      "At AP2T, we recognize that strength and speed will only take you so far in your sport. While both are extremely important, proper technique is equally critical. AP2T’s coaching staff includes professionals who currently work at the professional and Division-I collegiate levels. Their years of experience and mastery of their respective sports enable them to teach proper technique to athletes of all levels. All athletes who participate in sport-specific technical training receive personalized programs designed to help them master the technical demands of their sport.",
+    icon: <Trophy className="w-8 h-8 text-primary" />,
+    price:40
+  },
+
+  {
+    title: "Soccer Training",
+    description:
+      "AP2T offers team training for athletes of all ages and levels. Many of our current coaches work at the professional and Division-I levels, and all are fully licensed to coach youth soccer teams. Our coaches have achieved success at both the youth and professional levels, winning State, Regional, and National Championships along the way. Many also hold advanced coaching certifications. Please email us to learn more about our Soccer Team Training programs.",
+    icon: <Volleyball className="w-8 h-8 text-primary" />,
+    price:40
+  },
+  {
+    title: "sport-Specific Technical Training",
+    description:
+      "Our staff has coached and played at the highest levels of youth, high school, collegiate, and NFL competition, providing the knowledge, experience, and motivation needed for our players to perform at their best. Through our performance training, athletes improve their fitness, speed, technique, strength, and agility. Our program delivers high-quality training and personalized attention to help develop well-rounded, complete players. ",
+    icon: <Trophy className="w-8 h-8 text-primary" />,
+    price:40
+  },
+]
