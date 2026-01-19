f1c6a0d1eba4f7b0400fa24a1ab9b477074e78d4
ubaid1840 <ubaidrehman1840@gmail.com>
2026-01-19 14:36:28 +0500

    sidebar fixed

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -19,31 +19,41 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import { cn } from "@/lib/utils";
 
 
 export const company = {
   name: "AP2T",
   logo: "/images/logo.png",
+  favicon: "/favicon.png"
 };
 
 export default function AppSidebar() {
   const pathname = usePathname();
 
   const isMobile = useIsMobile();
-  const { toggleSidebar } = useSidebar();
-
+  const { toggleSidebar, state } = useSidebar();
 
 
   return (
     <Sidebar collapsible="icon">
       <SidebarHeader className="border-b">
         <div className="flex w-full align-center justify-center py-1">
-          <div className="w-35 h-9.25  rounded-full bg-black overflow-hidden">
-            <img
-              src={company.logo}
-              alt="AP2T LOGO"
-              className="w-full h-full "
-            />
+          <div className={cn("w-35 h-9.25   overflow-hidden", state === "expanded" && "rounded-full bg-black")}>
+            {state === "expanded" ?
+
+              <img
+                src={company.logo}
+                alt="AP2T LOGO"
+                className="w-full h-full "
+              />
+              :
+                <img
+                src={company.favicon}
+                alt="AP2T LOGO"
+                className="w-full h-full "
+              />
+            }
           </div>
         </div>
       </SidebarHeader>
@@ -85,19 +95,22 @@ export default function AppSidebar() {
         </ScrollArea>
       </SidebarContent>
 
-      <SidebarFooter className="border-t py-5">
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <div className="flex flex-col gap-1 w-full bg-primary rounded-xl p-4">
-              <p className="text-xs text-[#282828]">Need Help?</p>
-              <p className="text-md text-[#282828]">Contact Support</p>
-              <Button className="bg-black text-primary rounded-xl">
-                Get Support
-              </Button>
-            </div>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
+      {state === "expanded" &&
+
+        <SidebarFooter className="border-t py-5">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <div className="flex flex-col gap-1 w-full bg-primary rounded-xl p-4">
+                <p className="text-xs text-[#282828]">Need Help?</p>
+                <p className="text-md text-[#282828]">Contact Support</p>
+                <Button className="bg-black text-primary rounded-xl">
+                  Get Support
+                </Button>
+              </div>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      }
       <SidebarRail />
     </Sidebar>
   );
