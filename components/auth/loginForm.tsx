8ba2ef06f2b75960cec467a6b17e033350ca8561
Hammad2346 <HammadArif14244@gmail.com>
2026-01-29 11:47:30 +0500

    login signup datastate added

--- components/auth/loginForm.tsx
+++ components/auth/loginForm.tsx
@@ -1,16 +1,41 @@
+"use client"
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { useState } from "react";
 
 export default function LoginForm({ onClickSignup }: { onClickSignup: () => void }) {
+      const [logindata,setLoginData]=useState(
+        {
+          email:"",
+          password:"",
+        }
+      )
+
     const handlesubmit = () => {
-        console.log("submitting")
+        console.log(logindata)
     }
     return (
         <form className="space-y-5" onSubmit={handlesubmit}>
             <div className="flex flex-col justify-center items-center gap-5">
-                <Input required className="rounded" type="email" placeholder="Email" />
-                <Input required className="rounded" type="password" placeholder="Password" />
+                <Input required className="rounded" type="email" placeholder="Email" 
+                value={logindata.email}
+                onChange={(e) =>
+                        setLoginData((prev) => ({
+                          ...prev,
+                          email: e.target.value,
+                        }))
+                      }
+                />
+                <Input required className="rounded" type="password" placeholder="Password"
+                value={logindata.password}
+                onChange={(e) =>
+                        setLoginData((prev) => ({
+                          ...prev,
+                          password: e.target.value,
+                        }))
+                      }
+                />
                 <Link href="#">Forgot Password?</Link>
             </div>
 
