947cb8a99b241a6adefa9fbbb449199dfaf4889d
ubaid1840 <ubaidrehman1840@gmail.com>
2026-03-30 15:48:07 +0500

    cash and approval added

--- app/api/front-desk/route.ts
+++ app/api/front-desk/route.ts
@@ -68,163 +68,165 @@ export async function PUT(req: NextRequest) {
 
     const updatingRow = result.rows?.[0] ?? null
     if (updatingRow) {
-      const user_id = updatingRow?.user_id
-      const session_id = updatingRow?.session_id
-      let amount = 0
+      if (type === 'cash' || type === 'aproval') {
+        const user_id = updatingRow?.user_id
+        const session_id = updatingRow?.session_id
+        let amount = 0
 
-      const client = await pool.connect()
+        const client = await pool.connect()
 
-      try {
-        await client.query("BEGIN")
+        try {
+          await client.query("BEGIN")
 
-        /* ---------------- SESSION ---------------- */
+          /* ---------------- SESSION ---------------- */
 
-        const sessionResult = await client.query(
-          `SELECT id, price, apply_promotion, promotion_price, comped, max_players
+          const sessionResult = await client.query(
+            `SELECT id, price, apply_promotion, promotion_price, comped, max_players
        FROM sessions
        WHERE id = $1
        FOR UPDATE`,
-          [session_id]
-        )
-
-        const session = sessionResult.rows?.[0]
-
-        if (!session) {
-          await client.query("ROLLBACK")
-          return NextResponse.json({ message: "Session not found" }, { status: 404 })
-        }
-
-        /* ---------------- PLAYER ---------------- */
-
-        const playerResult = await client.query(
-          `SELECT id, square_customer_id, square_card_id
-       FROM users
-       WHERE id = $1`,
-          [user_id]
-        )
-
-        const player = playerResult.rows?.[0]
-
-        if (!player) {
-          await client.query("ROLLBACK")
-          return NextResponse.json({ message: "Player not found" }, { status: 404 })
-        }
-
-        /* ---------------- CHECK SESSION PLAYER ---------------- */
-
-        const playerCheck = await client.query(
-          `SELECT 1 FROM session_players
-       WHERE session_id = $1 AND user_id = $2`,
-          [session_id, user_id]
-        )
-
-        if (playerCheck.rows.length === 0) {
-          const countResult = await client.query(
-            `SELECT COUNT(*) FROM session_players WHERE session_id = $1`,
             [session_id]
           )
 
-          const currentPlayers = Number(countResult.rows[0].count)
-          const maxPlayers = Number(session.max_players)
+          const session = sessionResult.rows?.[0]
 
-          if (currentPlayers >= maxPlayers) {
+          if (!session) {
             await client.query("ROLLBACK")
-
-            return NextResponse.json(
-              { message: "Max players added in the session can not add more" },
-              { status: 409 }
-            )
+            return NextResponse.json({ message: "Session not found" }, { status: 404 })
           }
 
+          /* ---------------- PLAYER ---------------- */
 
-          amount = session.price
-          const now = moment();
-          if (session.comped) {
-            amount = 0;
-          } else if (
-            session.apply_promotion &&
-            session.promotion_price &&
-            session.promotion_end &&
-            moment(session.promotion_end).isAfter(now)
-          ) {
-            amount = session.promotion_price;
+          const playerResult = await client.query(
+            `SELECT id, square_customer_id, square_card_id
+       FROM users
+       WHERE id = $1`,
+            [user_id]
+          )
+
+          const player = playerResult.rows?.[0]
+
+          if (!player) {
+            await client.query("ROLLBACK")
+            return NextResponse.json({ message: "Player not found" }, { status: 404 })
           }
 
-          /* ---------------- INSERT PLAYER ---------------- */
-          await client.query(
-            `INSERT INTO session_players (session_id, user_id)
-         VALUES ($1, $2)`,
+          /* ---------------- CHECK SESSION PLAYER ---------------- */
+
+          const playerCheck = await client.query(
+            `SELECT 1 FROM session_players
+       WHERE session_id = $1 AND user_id = $2`,
             [session_id, user_id]
           )
 
-          if (session.comped) {
+          if (playerCheck.rows.length === 0) {
+            const countResult = await client.query(
+              `SELECT COUNT(*) FROM session_players WHERE session_id = $1`,
+              [session_id]
+            )
+
+            const currentPlayers = Number(countResult.rows[0].count)
+            const maxPlayers = Number(session.max_players)
+
+            if (currentPlayers >= maxPlayers) {
+              await client.query("ROLLBACK")
+
+              return NextResponse.json(
+                { message: "Max players added in the session can not add more" },
+                { status: 409 }
+              )
+            }
+
+
+            amount = session.price
+            const now = moment();
+            if (session.comped) {
+              amount = 0;
+            } else if (
+              session.apply_promotion &&
+              session.promotion_price &&
+              session.promotion_end &&
+              moment(session.promotion_end).isAfter(now)
+            ) {
+              amount = session.promotion_price;
+            }
+
+            /* ---------------- INSERT PLAYER ---------------- */
             await client.query(
-              `INSERT INTO payments
+              `INSERT INTO session_players (session_id, user_id)
+         VALUES ($1, $2)`,
+              [session_id, user_id]
+            )
+
+            if (session.comped) {
+              await client.query(
+                `INSERT INTO payments
            (session_id, user_id, amount, status, paid_at, method)
            VALUES ($1,$2,$3,$4,$5,$6)`,
-              [session_id, user_id, amount, "comped", new Date(), "Nil"]
-            )
-          } else {
-            await client.query(
-              `INSERT INTO payments
+                [session_id, user_id, amount, "comped", new Date(), "Nil"]
+              )
+            } else {
+              await client.query(
+                `INSERT INTO payments
            (session_id, user_id, amount, status)
            VALUES ($1,$2,$3,$4)`,
-              [session_id, user_id, amount, "pending"]
-            )
+                [session_id, user_id, amount, "pending"]
+              )
+            }
           }
-        }
 
-        const paymentResult = await client.query(
-          `SELECT status
+          const paymentResult = await client.query(
+            `SELECT status
        FROM payments
        WHERE session_id = $1 AND user_id = $2`,
-          [session_id, user_id]
-        )
-        const payment = paymentResult.rows?.[0]
+            [session_id, user_id]
+          )
+          const payment = paymentResult.rows?.[0]
 
-        if (!payment) {
-          throw new Error("Payment record missing")
+          if (!payment) {
+            throw new Error("Payment record missing")
+          }
+
+          await client.query("COMMIT")
+
+        } catch (error: any) {
+          await client.query("ROLLBACK")
+
+          console.log("Error:", error)
+
+          return NextResponse.json(
+            {
+              message:
+                error?.response?.data?.message ||
+                error?.message ||
+                "Failed to process payment",
+            },
+            { status: 500 }
+          )
+        } finally {
+          client.release()
         }
-
-        await client.query("COMMIT")
- 
-      } catch (error: any) {
-        await client.query("ROLLBACK")
-
-        console.log("Error:", error)
-
-        return NextResponse.json(
-          {
-            message:
-              error?.response?.data?.message ||
-              error?.message ||
-              "Failed to process payment",
-          },
-          { status: 500 }
-        )
-      } finally {
-        client.release()
       }
-    }
 
-    if (type === "cash") {
-      await pool.query(`
+      if (type === "cash") {
+        await pool.query(`
   UPDATE payments
   SET method = 'Cash', status = 'paid', paid_at = $1, paid_by = $2, transaction_id = $3 
   WHERE user_id = $4 AND session_id = $5
 `, [new Date(), updatingRow?.user_id, moment().valueOf().toString(), updatingRow?.user_id, updatingRow?.session_id])
-    } 
+      }
 
-    await TriggerFirebaseApprovals("user")
-    await TriggerFirebaseApprovals("admin")
+      await TriggerFirebaseApprovals("user")
+      await TriggerFirebaseApprovals("admin")
 
-    return new Response(
-      JSON.stringify({
-        message: "Status updated successfully",
-        data: result.rows[0],
-      }),
-      { status: 200 }
-    );
+      return new Response(
+        JSON.stringify({
+          message: "Status updated successfully",
+          data: result.rows[0],
+        }),
+        { status: 200 }
+      );
+    }
   } catch (error) {
     console.error("PUT /front-desk error:", error);
     return new Response(
