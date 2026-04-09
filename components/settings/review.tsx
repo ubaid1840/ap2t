7eb3f4a1ca5bcde854e40c371d91db02a31a02c1
ubaid1840 <ubaidrehman1840@gmail.com>
2026-04-09 13:58:54 +0500

    review updated

--- components/settings/review.tsx
+++ components/settings/review.tsx
@@ -1,4 +1,4 @@
-import { Link } from "lucide-react";
+import { Link, LinkIcon } from "lucide-react";
 import { TabsContent } from "../ui/tabs";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
@@ -6,6 +6,7 @@ import { Spinner } from "../ui/spinner";
 import { useState } from "react";
 import axios from "@/lib/axios";
 import { useAuth } from "@/contexts/auth-context";
+import { toast } from "sonner";
 
 
 export default function () {
@@ -22,6 +23,7 @@ export default function () {
                 user_id: user?.id
             })
             const link = res.data.link
+           toast.success("Link created successfully! It will expire in 24 hours.");
             setLink(link)
         } finally {
             setLinkLoading(false)
@@ -32,13 +34,28 @@ export default function () {
         <TabsContent value="Review" className="space-y-4">
             <div className="flex justify-center w-full">
                 <div className="flex items-center gap-2">
-                    <Link />
                     <Input
                         className="w-2xl h-10"
                         value={link}
                         onChange={(e) => setLink(e.target.value)}
                     />
-                    <Button onClick={generateLink}>{linkLoading && <Spinner className=" text-black h-5 w-5" />}Generate Link</Button>
+                    {link && (
+                        <button
+                            type="button"
+                            onClick={async () => {
+                                try {
+                                    await navigator.clipboard.writeText(link);
+                                   toast.success("Link copied")
+                                } catch (err) {
+                                    console.log("Failed to copy: ", err);
+                                }
+                            }}
+                            className="p-1 hover:bg-gray-700 rounded"
+                        >
+                            <LinkIcon />
+                        </button>
+                    )}
+                    <Button disabled={linkLoading} onClick={generateLink}>{linkLoading && <Spinner className=" text-black h-5 w-5" />}Generate Link</Button>
                 </div>
             </div>
         </TabsContent>
